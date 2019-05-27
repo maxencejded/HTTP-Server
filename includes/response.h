81c9c7a58621a0820d43b753c482fc63c03cd5d0
Jules Spender <pro.jspender@gmail.com>
2019-05-27 12:25:03 -0700

    Reponse creation

---
+++ includes/response.h
@@ -0,0 +1,13 @@
+#ifndef RESPONSE_H
+# define RESPONSE_H
+
+# include <netdb.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <unistd.h>
+# include <sys/stat.h>
+
+# include "libft.h"
+
+#endif /* RESPONSE_H */
