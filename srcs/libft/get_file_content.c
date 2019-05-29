32a2eac699a2967ed2ce962a05ff256967b50242
Jules Spender <pro.jspender@gmail.com>
2019-05-28 21:07:15 -0700

    Removed ugly code

--- srcs/libft/get_file_content.c
+++ srcs/libft/get_file_content.c
@@ -4,15 +4,10 @@
 
 uint8_t		*get_file_content(uint64_t *file_size, char *filename)
 {
-	uint8_t		*content;
 	struct stat	st;
 
 	if (stat(filename, &st) != 0 && (*file_size = 0) == 0)
 		return (NULL);
 	*file_size = st.st_size;
-	if ((content = malloc(sizeof(uint8_t) * 2)) == NULL)
-		return (NULL);
-	content[0] = '1';
-	content[1] = '\0';
-	return (content);
+	return ((void *)1);
 }
